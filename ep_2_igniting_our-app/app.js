8cbd7d03fbe17bc75eece72c313a4264ef0dee85
Ishwaridutt <ishwaridutt8@gmail.com>
2024-01-08 10:42:20 +0530

    ep4_talk_is_cheap_show_me_code

--- ep_2_igniting_our-app/app.js
+++ ep_2_igniting_our-app/app.js
@@ -1,24 +1,119 @@
-// const heading = React.createElement(
-//     'h1',
-//     { id: 'heading' },
-//     'Hello from React'
-// )
+// Chapter 04 - Talk is Cheap, show me the code
 
-// console.log(heading);
 
-const heading = React.createElement(
-    'div', { id: 'parent' }, [
-        React.createElement('div', {id: 'child1'}, [
-            React.createElement('h1', {}, 'I am an h1 tag'),
-            React.createElement('h1', {}, 'I am an h1 tag')
-        ]),
-        React.createElement('div', {id: 'child2'}, [
-            React.createElement('h1', {}, 'I am an h1 tag'),
-            React.createElement('h1', {}, 'I am an h1 tag')
-        ])
-    ]
-);
+import React from 'react';
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading)
+import resData from './data.json'
+
+/* 
+my food app structure
+
+    Header
+        - logo
+        - nav items
+        - cart
+    Body
+        - search bar
+        - restaurant list
+            - restaurant card
+                - image
+                - NAME
+                - rating
+    Footer
+        - links
+        - copy right
+*/
+
+
+// Title component for display logo
+const Title = () => {
+    <a>
+      <img className='logo' />  
+    </a>
+}
+
+
+const restaurantData = resData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+console.log(restaurantData);
+
+// Header component for header section: Logo, Nav Items
+const Header = () => {
+    return (
+        <div className='header' >
+            <div className='logo-container' >
+                <img 
+                className='logo'
+                src='https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?size=2'
+                />
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+
+}
+
+const RestaurantCard = (props) => {
+    const resData = props.resData.info
+    return (
+        <div className='res-card' >
+            <img 
+                className='res-logo'
+                alt='res-logo'
+                src={
+                    'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/' + resData.cloudinaryImageId
+                }
+            />
+            <h3>{resData.name}</h3>
+            <h3>{resData.cuisines.join(', ')}</h3>
+            <h3>{resData.avgRating}</h3>
+            <h3>{resData.costForTwo}</h3>
+            <h3>{resData.sla.deliveryTime}</h3>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='search' >Search</div>
+            <div className='res-container' >
+                {
+                    restaurantData.map( restaurant => <RestaurantCard resData = {restaurant} />)
+                }
+            </div>
+        </div>
+    )
+}
+
+
+const AppLayout = () => {
+    return (
+        <div className='app' >
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
+
+
+
+
+
+
+
+
+
+
 
